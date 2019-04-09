da4a58441a0e79853ff45d73b463d5be0946b5e4
prhyder <hyderpr@gmail.com>
2019-04-09 11:47:44 -0400

    Updates

---
+++ precache-manifest.90297c6c06bb6a42419002af02eb3171.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5386fd8ce4acf794f5f05d669525c08",
+    "url": "/RPG_App/static/media/tallPines.c5386fd8.png"
+  },
+  {
+    "revision": "6c87ae89fa618d709d1b80cf6a81f0d5",
+    "url": "/RPG_App/static/media/parchment.6c87ae89.png"
+  },
+  {
+    "revision": "83ad6f050e77f0500d12d9ba7d13bdae",
+    "url": "/RPG_App/static/media/borderBoards.83ad6f05.png"
+  },
+  {
+    "revision": "9c36b9ec85f1cb20d9102162ca291946",
+    "url": "/RPG_App/static/media/availablePointsBackground.9c36b9ec.png"
+  },
+  {
+    "revision": "980c0ba05b3326e1f6b5",
+    "url": "/RPG_App/static/js/runtime~main.980c0ba0.js"
+  },
+  {
+    "revision": "36dd5dd2ab705b0e4146",
+    "url": "/RPG_App/static/js/main.36dd5dd2.chunk.js"
+  },
+  {
+    "revision": "a35c14269d3c7a0fc6a7",
+    "url": "/RPG_App/static/js/2.a35c1426.chunk.js"
+  },
+  {
+    "revision": "36dd5dd2ab705b0e4146",
+    "url": "/RPG_App/static/css/main.0dfbbff5.chunk.css"
+  },
+  {
+    "revision": "f2e7cae1bd7a67c54ef3174c224e9cb2",
+    "url": "/RPG_App/index.html"
+  }
+];
